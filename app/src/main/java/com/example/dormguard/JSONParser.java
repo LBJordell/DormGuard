5be1e2caac1dabf754dfa1c84f9d3a34caa91cf9
Lukas <LBJordell@gmail.com>
2015-12-05 12:00:11 +0100

    Created VideoLibrary and LiveStream Java class

---
+++ app/src/main/java/com/example/dormguard/JSONParser.java
@@ -0,0 +1,7 @@
+package com.example.dormguard;
+
+/**
+ * Created by Lukas on 05/12/15.
+ */
+public class JSONParser {
+}
--- app/src/main/java/com/example/dormguard/Login.java
+++ app/src/main/java/com/example/dormguard/Login.java
@@ -2,7 +2,7 @@ package com.example.dormguard;
 
 /**
  * Created by Lukas on 04/12/15.
- * heeeeeeeeeeeeeeeeeej
  */
+
 public class Login {
 }
